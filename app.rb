26ab8bfa5625fffe847e7db93ab22b0d50b597de
sgowda91 <sunil.gowda@kellogg.northwestern.edu>
2020-03-19 18:42:19 +0000

    update

--- app.rb
+++ app.rb
@@ -4,6 +4,7 @@ require "sinatra/reloader" if development?                                      
 require "sequel"                                                                      #
 require "logger"                                                                      #
 require "twilio-ruby"                                                                 #
+require "geocoder"                                                                    #
 require "bcrypt"                                                                      #
 connection_string = ENV['DATABASE_URL'] || "sqlite://#{Dir.pwd}/development.sqlite3"  #
 DB ||= Sequel.connect(connection_string)                                              #
@@ -16,6 +17,11 @@ after { puts; }                                                                 
 
 events_table = DB.from(:events)
 users_table = DB.from(:users)
+searches_table = DB.from(:searches)
+
+before do
+    @current_user = users_table.where(id: session["user_id"]).to_a[0]
+end
 
 get "/" do
     view "home"
@@ -39,7 +45,58 @@ post "/users/create" do
             email: params["email"],
             password: BCrypt::Password.create(params["password"])
         )
+        
+        redirect "/logins/new"
+    end
+end
 
-        redirect "/" #i need to update this
+get "/logins/new" do
+    view "new_login"
+end
+
+post "/logins/create" do
+    puts "params: #{params}"
+
+    # step 1: user with the params["email"] ?
+    @user = users_table.where(email: params["email"]).to_a[0]
+
+    if @user
+        # step 2: if @user, does the encrypted password match?
+        if BCrypt::Password.new(@user[:password]) == params["password"]
+            # set encrypted cookie for logged in user
+            session["user_id"] = @user[:id]
+            session["user_email"] = @user[:email]
+            redirect "/"
+        else
+            view "create_login_failed"
+        end
+    else
+        view "create_login_failed"
+    end
+end
+
+get "/logout" do
+    # remove encrypted cookie for logged out user
+    session["user_id"] = nil
+    redirect "/logins/new"
+end
+
+get "/search/city" do
+    results = Geocoder.search(params["q"])
+    @lat_long = results.first.coordinates
+    @location = params["q"]
+
+    view "city_search"
+end
+
+post "/search/save" do
+    if session["user_id"]
+        searches_table.insert(
+            user_id: session["user_id"],
+            location: "location"
+        )
+        view "test"
+    else
+        view "error"
     end
 end
--- createdb.rb
+++ createdb.rb
@@ -5,18 +5,25 @@ DB = Sequel.connect(connection_string)                                          
 #######################################################################################
 
 # Database schema - this should reflect your domain model
-DB.create_table! :events do
-  primary_key :id
-  String :title
-  String :description, text: true
-  String :date
-  String :location
-end
+# DB.create_table! :events do
+#   primary_key :id
+#   String :title
+#   String :description, text: true
+#   String :date
+#   String :location
+# end
 DB.create_table! :users do
   primary_key :id
   String :name
   String :email
   String :password
+end
+
+DB.create_table! :searches do
+  primary_key :id
+  String :user_id
+  String :location
+  String :email
 end
 
 # Insert initial (seed) data
